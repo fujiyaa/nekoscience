d340c6043f8f16c4eb056ab47c0f682170a706d8
Fujiya <dedmozay2002@gmail.com>
2025-11-15 13:37:31 +0300

    upd ps1

--- start_bot.ps1
+++ start_bot.ps1
@@ -1,2 +1,6 @@
-& ".\bot\src\venv\Scripts\Activate.ps1"
-python .\bot\src\main.py
+try {
+    & ".\bot\src\venv\Scripts\Activate.ps1"
+    python .\bot\src\main.py
+} finally {
+    deactivate
+}
--- start_web.ps1
+++ start_web.ps1
@@ -1,3 +1,9 @@
-& ".\web\src\venv\Scripts\Activate.ps1"
-Set-Location ".\web\src"
-python -m uvicorn main:app --host 127.0.0.1 --port 8000 --reload
+try {
+    & ".\web\src\venv\Scripts\Activate.ps1"
+    Set-Location ".\web\src"
+    python -m uvicorn main:app --host 127.0.0.1 --port 8000 --reload
+} finally {
+    deactivate
+    Set-Location ".\.."
+    Set-Location ".\.."
+}
