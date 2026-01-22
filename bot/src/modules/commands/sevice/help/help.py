d2020ac09ee54a00ad01b5869860e0943c34b3dd
Fujiya <dedmozay2002@gmail.com>
2026-01-23 00:22:55 +0300

    fix typo(s)

--- bot/src/modules/commands/sevice/help/help.py
+++ bot/src/modules/commands/sevice/help/help.py
@@ -44,7 +44,7 @@ async def help(update: Update, context: ContextTypes.DEFAULT_TYPE, user_request)
                 MessageEntity(
                     type="expandable_blockquote",
                     offset=0,                     
-                    length=len(help_text)+20 # extra offset cos idk         
+                    length=len(help_text)+10 # extra offset cos idk         
                 )
             ]        
             max_attempts = 3
