f05230889f4454e20c07cf115c09e81fed4879ff
Fujiya <dedmozay2002@gmail.com>
2026-02-03 23:13:34 +0300

    add entity

--- bot/src/modules/commands/osu/leaderboard_chat/send.py
+++ bot/src/modules/commands/osu/leaderboard_chat/send.py
@@ -1,25 +1,47 @@
 
 
 
-from telegram import Update
+from telegram import Update, MessageEntity
 
-from ....actions.messages import safe_send_message
+from ....actions.messages import safe_send_message, safe_edit_query
 
 
 
 async def send(update: Update, stats_batch: dict = None, caption: str = ''):
     if not stats_batch:
-        await safe_send_message(
+        try:
+            await safe_edit_query(
+                update.callback_query, 
+                "Нет данных",
+                parse_mode="HTML"
+            )
+        except:
+            await safe_send_message(
                 update,
                 "Нет данных",
-                parse_mode="HTML",
-                # reply_markup=pagination
+                parse_mode="HTML"
             )     
         return
-
-    await safe_send_message(
-                update,
-                caption,
-                parse_mode="HTML",
-                # reply_markup=pagination
-            )         
+    
+    try:
+        entities = [
+            MessageEntity(
+                type="expandable_blockquote",
+                offset=0,                     
+                length=len(caption)+4     
+            )
+        ]        
+        
+        await safe_edit_query(
+            update.callback_query, 
+            caption,
+            parse_mode="HTML",
+            entities=entities
+        )
+    except:
+        await safe_send_message(
+            update,
+            caption,
+            parse_mode="HTML",
+            # reply_markup=pagination
+        )         
